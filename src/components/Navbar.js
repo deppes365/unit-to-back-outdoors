6b7388a2585c6e628c377458f06f124735a5a0fb
Darren Eppes <deppes365@gmail.com>
2022-08-14 22:44:45 -0400

    menu added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,33 @@
+import { click } from '@testing-library/user-event/dist/click';
 import React from 'react';
 import { useState } from 'react';
 
 function Navbar() {
     const [menuActive, setMenuActive] = useState(false);
+	const [pageInView, setPageInView] = useState('home')
 
     const openMobileMenu = () => {
         setMenuActive(prevState => !prevState)
     }
 
-	
-
-
-
 	return (
 		<div className="navBar">
 			<div className="container">
 				<div className="logo">UTBO</div>
-				<ul className={`navLinks ${menuActive ? 'active' : ''}`}>
+				<ul className={`navLinks ${menuActive ? 'active' : ''}`} onClick={(e) => {
+					const clickedElement = e.target.className
+					if(clickedElement.includes('navLinks')) {
+						setMenuActive(false)
+					} if(clickedElement.includes('navLink')) {
+						// Close menu
+						// Scroll to page
+					}
+				}}>
 					<li className='navLink'>Home</li>
 					<li className='navLink'>About</li>
 					<li className='navLink'>Events</li>
 					<li className='navLink'>Gallery</li>
+					<li className='email'>unittobackoutdoors@gmail.com</li>
 				</ul>
 				<div className={`hamburger ${menuActive ? 'active' : ''}`} onClick={openMobileMenu}>
 					<div className="topLine"></div>
